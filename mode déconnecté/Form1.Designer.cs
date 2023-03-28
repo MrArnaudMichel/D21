4eab69aa27d4bd4e2b86d25d2d49d16f814e4845
arnaudmichel <arnaudmichel@IUTRS6>
2023-03-28 10:19:47 +0200

    Ajoutez des fichiers projet.

---
+++ mode déconnecté/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace mode_déconnecté
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cbbid = new System.Windows.Forms.ComboBox();
+            this.cbbDestinations = new System.Windows.Forms.ComboBox();
+            this.btnload = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnrecherche = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.cbbClient = new System.Windows.Forms.ComboBox();
+            this.cbbDest = new System.Windows.Forms.ComboBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.cbbid);
+            this.tabPage1.Controls.Add(this.cbbDestinations);
+            this.tabPage1.Controls.Add(this.btnload);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Destinations";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Nombre de voyage select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Nombre voyages boucle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cbbid
+            // 
+            this.cbbid.FormattingEnabled = true;
+            this.cbbid.Location = new System.Drawing.Point(171, 74);
+            this.cbbid.Name = "cbbid";
+            this.cbbid.Size = new System.Drawing.Size(121, 21);
+            this.cbbid.TabIndex = 2;
+            // 
+            // cbbDestinations
+            // 
+            this.cbbDestinations.FormattingEnabled = true;
+            this.cbbDestinations.Location = new System.Drawing.Point(171, 47);
+            this.cbbDestinations.Name = "cbbDestinations";
+            this.cbbDestinations.Size = new System.Drawing.Size(121, 21);
+            this.cbbDestinations.TabIndex = 1;
+            // 
+            // btnload
+            // 
+            this.btnload.Location = new System.Drawing.Point(171, 17);
+            this.btnload.Name = "btnload";
+            this.btnload.Size = new System.Drawing.Size(269, 23);
+            this.btnload.TabIndex = 0;
+            this.btnload.Text = "Charger base de données";
+            this.btnload.UseVisualStyleBackColor = true;
+            this.btnload.Click += new System.EventHandler(this.btnload_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Reservations";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 166);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(755, 166);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnrecherche);
+            this.groupBox1.Controls.Add(this.btnadd);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.cbbClient);
+            this.groupBox1.Controls.Add(this.cbbDest);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(756, 153);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Recherche par criteres";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnrecherche
+            // 
+            this.btnrecherche.Location = new System.Drawing.Point(464, 95);
+            this.btnrecherche.Name = "btnrecherche";
+            this.btnrecherche.Size = new System.Drawing.Size(189, 23);
+            this.btnrecherche.TabIndex = 8;
+            this.btnrecherche.Text = "Lancer recherche";
+            this.btnrecherche.UseVisualStyleBackColor = true;
+            this.btnrecherche.Click += new System.EventHandler(this.btnrecherche_Click);
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(464, 40);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(189, 23);
+            this.btnadd.TabIndex = 7;
+            this.btnadd.Text = "Nouveau voyage";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(77, 43);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "par destinations";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // cbbClient
+            // 
+            this.cbbClient.FormattingEnabled = true;
+            this.cbbClient.Location = new System.Drawing.Point(234, 96);
+            this.cbbClient.Name = "cbbClient";
+            this.cbbClient.Size = new System.Drawing.Size(200, 21);
+            this.cbbClient.TabIndex = 6;
+            this.cbbClient.Visible = false;
+            // 
+            // cbbDest
+            // 
+            this.cbbDest.FormattingEnabled = true;
+            this.cbbDest.Location = new System.Drawing.Point(234, 41);
+            this.cbbDest.Name = "cbbDest";
+            this.cbbDest.Size = new System.Drawing.Size(200, 21);
+            this.cbbDest.TabIndex = 0;
+            this.cbbDest.Visible = false;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(77, 98);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(69, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "par client";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(234, 69);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.Visible = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(77, 71);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(65, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "par date";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cbbid;
+        private System.Windows.Forms.ComboBox cbbDestinations;
+        private System.Windows.Forms.Button btnload;
+        private System.Windows.Forms.ComboBox cbbDest;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox cbbClient;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnrecherche;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ mode déconnecté/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mode_déconnecté
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string chcon = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=U:\Voyages2010.mdb";
+        DataSet ds = new DataSet();
+        OleDbConnection conn = new OleDbConnection();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            foreach(DataRow d in ds.Tables["tblReservations"].Rows)
+            {
+                if (d["CodeVoyage"].ToString() == cbbDestinations.SelectedValue.ToString())
+                    i++;
+            }
+            MessageBox.Show(i.ToString());
+        }
+
+        private void btnload_Click(object sender, EventArgs e)
+        {
+            cbbDestinations.DataSource = ds.Tables["tblVoyages"];
+            cbbDestinations.DisplayMember = "Destination";
+            cbbDestinations.ValueMember = "CodeVoyage";
+            cbbDestinations.SelectedIndex = -1;
+
+            foreach (DataRow d in ds.Tables["tblReservations"].Rows)
+            {
+                cbbid.Items.Add(d["CodeVoyage"].ToString() + d["NumClient"].ToString());
+            }
+        }
+
+        private void ChargementDsLocal()
+        {
+            conn.Open();
+            DataTable dt = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] {null, null, null, "Table"});
+            foreach (DataRow dr in dt.Rows)
+            {
+                string nomTable = dr["TABLE_NAME"].ToString();
+                OleDbDataAdapter da = new OleDbDataAdapter("SELECT * FROM " + nomTable, conn);
+                da.Fill(ds, nomTable);
+            }
+            conn.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn.ConnectionString = chcon;
+            ChargementDsLocal();
+            cbbDest.DataSource = ds.Tables["tblVoyages"];
+            cbbDest.DisplayMember = "Destination";
+            cbbDest.ValueMember = "CodeVoyage";
+            cbbDest.SelectedIndex = -1;
+
+            cbbClient.DataSource = ds.Tables["tblClients"];
+            cbbClient.DisplayMember = "NomClient";
+            cbbClient.ValueMember = "NumClient";
+            cbbClient.SelectedIndex = -1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            cbbDest.Visible = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1.Visible = checkBox2.Checked;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            cbbClient.Visible = checkBox3.Checked;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnrecherche_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            string request = "";
+            bool first = true;
+            if (checkBox1.Checked)
+            {
+                request += "CodeVoyage = '" + cbbDest.SelectedValue.ToString() + "' ";
+                first = false;
+            }
+            if (checkBox2.Checked)
+            {
+                if (!first)
+                {
+                    request += "AND ";
+                }
+                first = false;
+                request += "DateReservation = #"+dateTimePicker1.Value.Month.ToString()+ "/" + dateTimePicker1.Value.Day.ToString() + "/" + dateTimePicker1.Value.Year.ToString() + "# ";
+            }
+            if (checkBox3.Checked)
+            {
+                if (!first)
+                {
+                    request += "AND ";
+                }
+                request += "NumClient = '" + cbbClient.SelectedValue.ToString() + "'";
+            }
+            MessageBox.Show(request);
+            DataRow[] drs = ds.Tables["tblReservations"].Select(request);
+            dt = drs.CopyToDataTable();
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(ds.Tables["tblReservations"].Select("CodeVoyage = '" + cbbDestinations.SelectedValue + "'").Length.ToString());
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            string request = "INSERT INTO tblReservations(NumClient, CodeVoyage, DateReservation) values('" + cbbDest.SelectedValue.ToString() + "', #" + dateTimePicker1.Value.Month.ToString() + "/" + dateTimePicker1.Value.Day.ToString() + "/" + dateTimePicker1.Value.Year.ToString() + "#, '" + cbbClient.SelectedValue.ToString() + "')";
+            DataRow datarow = new 
+        }
+    }
+}
